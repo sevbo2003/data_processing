d24b950612240d61f34ea70f33c8e73f61084572
Abdusamad Malikov <sevbofx@gmail.com>
2023-02-15 16:07:02 +0500

    `Language` model created

--- DataProcessingSite/presentation/models.py
+++ DataProcessingSite/presentation/models.py
@@ -17,4 +17,27 @@ class EnglishName(models.Model):
     class Meta:
         verbose_name_plural = "English Names"
         ordering = ['-id']
-        
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Languages"
+        ordering = ['-id']
+
+
+class PresentationLanguage(models.Model):
+    lagnuage = models.ForeignKey(Language, on_delete=models.CASCADE)
+    english_name = models.ForeignKey(EnglishName, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Presentation Languages"
+        ordering = ['-id']
