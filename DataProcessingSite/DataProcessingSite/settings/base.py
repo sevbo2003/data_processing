91520d6282e316502348e560b27e30e987df77cd
Abdusamad Malikov <sevbofx@gmail.com>
2023-02-15 18:42:43 +0500

    `bio`

--- DataProcessingSite/DataProcessingSite/settings/base.py
+++ DataProcessingSite/DataProcessingSite/settings/base.py
@@ -83,12 +83,12 @@ DATABASES = {
         "PORT": 5432,
     }
 }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
