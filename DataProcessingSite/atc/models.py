a091c592972fe26c00ceffc95a20b5cfdc72dbaf
Abdusamad Malikov <sevbofx@gmail.com>
2023-02-15 20:20:38 +0500

    `Atc` model created

--- DataProcessingSite/atc/models.py
+++ DataProcessingSite/atc/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from presentation.models import Language
 
-# Create your models here.
+
+class Atc(models.Model):
+    atc_code = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.atc_code
+
+    class Meta:
+        verbose_name_plural = "ATC"
+        ordering = ['atc_code']
