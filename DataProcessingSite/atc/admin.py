4282f0b68f528ad7901ea6def55b177034357fba
Abdusamad Malikov <sevbofx@gmail.com>
2023-02-15 20:20:57 +0500

    `AtcAdmin` created

--- DataProcessingSite/atc/admin.py
+++ DataProcessingSite/atc/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class AtcAdmin(admin.ModelAdmin):
+    list_display = ('atc_code',)
+    search_fields = ('atc_code',)
+    list_filter = ('atc_code',)
+    ordering = ('atc_code',)
+
+admin.site.register(Atc, AtcAdmin)
