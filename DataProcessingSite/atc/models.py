30d9f2492353bca0fad5baff598b9eaee2dd0f66
Abdusamad Malikov <sevbofx@gmail.com>
2023-02-15 20:23:30 +0500

    `AtcLanguage` model created

--- DataProcessingSite/atc/models.py
+++ DataProcessingSite/atc/models.py
@@ -11,3 +11,17 @@ class Atc(models.Model):
     class Meta:
         verbose_name_plural = "ATC"
         ordering = ['atc_code']
+
+
+class AtcLanguage(models.Model):
+    atc = models.ForeignKey(Atc, on_delete=models.CASCADE)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "ATC Languages"
+        ordering = ['atc']
+        
