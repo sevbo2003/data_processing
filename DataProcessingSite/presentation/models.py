6632beed25bfe84a3856bb7a839500b068411df1
Abdusamad Malikov <sevbofx@gmail.com>
2023-02-15 16:01:07 +0500

    `EnglishName` model created

--- DataProcessingSite/presentation/models.py
+++ DataProcessingSite/presentation/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from main.models import Presentation
+from main.csv_xl import FileReader
+from lxml import etree
+from xml.etree import ElementTree
+import uuid
+import os
+import pandas as pd
 
-# Create your models here.
+
+class EnglishName(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "English Names"
+        ordering = ['-id']
+        
