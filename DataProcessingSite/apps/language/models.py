61001809417cb2732b6d3d8d9b0f2454d1f47508
Abdusamad Malikov <sevbofx@gmail.com>
2023-02-22 00:32:04 +0500

    `Product` model created

--- DataProcessingSite/apps/language/models.py
+++ DataProcessingSite/apps/language/models.py
@@ -14,3 +14,32 @@ def csv_column_names():
             'reimbursement', 'pack_size', 'presentation', 'route', 'is_commercial', 'is_narcotic',
             'active_ingredient_1', 'active_ingredient_2', 'active_ingredient_3', 'active_ingredient_4',
             'strength_1', 'strength_2', 'strength_3', 'strength_4']
+
+
+class Product(models.Model):
+    language = models.ForeignKey(Language, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=100, blank=True, null=True)
+    eu_code = models.CharField(max_length=100, blank=True, null=True)
+    national_registration_number = models.CharField(max_length=100, blank=True, null=True)
+    date_of_registration = models.CharField(max_length=100 ,blank=True, null=True)
+    registration_expiration = models.CharField(max_length=100 ,blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    atc_code = models.CharField(max_length=100)
+    ma_holder = models.CharField(max_length=100, blank=True, null=True)
+    prescription_status = models.CharField(max_length=100, blank=True, null=True)
+    link_to_spc = models.CharField(max_length=100, blank=True, null=True)
+    link_to_pil = models.CharField(max_length=100, blank=True, null=True)
+    reimbursement = models.CharField(max_length=100, blank=True, null=True)
+    pack_size = models.CharField(max_length=100, blank=True, null=True)
+    presentation = models.CharField(max_length=100, blank=True, null=True)
+    route = models.CharField(max_length=100, blank=True, null=True)
+    is_commercial = models.BooleanField(default=False, blank=True, null=True)
+    is_narcotic = models.BooleanField(default=False, blank=True, null=True)
+    active_ingredient_1 = models.CharField(max_length=100, blank=True, null=True)
+    active_ingredient_2 = models.CharField(max_length=100, blank=True, null=True)
+    active_ingredient_3 = models.CharField(max_length=100, blank=True, null=True)
+    active_ingredient_4 = models.CharField(max_length=100, blank=True, null=True)
+    strength_1 = models.CharField(max_length=100, blank=True, null=True)
+    strength_2 = models.CharField(max_length=100, blank=True, null=True)
+    strength_3 = models.CharField(max_length=100, blank=True, null=True)
+    strength_4 = models.CharField(max_length=100, blank=True, null=True)
